dac0574bf8f03909c6c4d7afcc0e90301dda8c6f
silvicardo <silvicardo@gmail.com>
2018-11-30 16:23:57 +0100

    progress dots

--- main.js
+++ main.js
@@ -13,19 +13,39 @@ $('.slider_cnt .fas').click(function(){
 });
 
 function slideVerso(direzione) {
-
+  //immagini
   var containerImmagini = $('.slider_cnt .slider_images_cnt');
   var immagineAttuale = $('.slider_cnt .slider_img.active');
   var primaImmagine = $('.slider_cnt img:first-child');
   var ultimaImmagine = $('.slider_cnt img:last-child');
   var tutteLeImmagini = $('.slider_cnt .slider_img');
 
+  //icone pallini
+  var tuttiIPallini =$('.fa-circle');
+  var pallinoAttuale = $('.fas.fa-circle');
+  var primoPallino = $('.slider_indicators i:first-child');
+  var ultimoPallino = $('.slider_indicators i:last-child');
+
   tutteLeImmagini.removeClass('active');
+  pallinoAttuale.removeClass('fas').addClass('far');
+
 
   if (direzione == 'destra') {
-    (immagineAttuale.next().length == 1 ) ? immagineAttuale.next().addClass('active') : primaImmagine.addClass('active');
+    if (immagineAttuale.next().length == 1 ) {
+       immagineAttuale.next().addClass('active');
+       pallinoAttuale.next().addClass('fas').removeClass('far');
+      } else {
+        primaImmagine.addClass('active');
+        primoPallino.addClass('fas').removeClass('far');
+        }
   } else if (direzione == 'sinistra') {
-    (immagineAttuale.prev().length == 1) ? immagineAttuale.prev().addClass('active') : ultimaImmagine.addClass('active');
+    if (immagineAttuale.prev().length == 1) {
+      immagineAttuale.prev().addClass('active');
+      pallinoAttuale.prev().addClass('fas').removeClass('far');
+      } else {
+        ultimaImmagine.addClass('active');
+        ultimoPallino.addClass('fas').removeClass('far');
+      }
   }
 
 }
